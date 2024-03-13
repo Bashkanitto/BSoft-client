ee19bed17fcbfda6e43e9daa26f0ed644a875a1f
aidyn <aidyn.meiramkhanulu@gmail.com>
2024-03-13 20:55:54 +0500

    fix team section paddings on mobile

--- src/utils/TeamAccordion2.jsx
+++ src/utils/TeamAccordion2.jsx
@@ -9,50 +9,53 @@ export const TeamAccordion2 = ({ list }) => {
 	};
 
 	return (
-		<>
+		<div>
 			{list.map((item, index) => (
-				<button
-					key={index}
-					onClick={() => toggleTab(index)}
-					style={{
-						height: activeQuestionId === index ? '400px' : '80px',
-						flexDirection: item.reverse == true ? 'row-reverse' : 'row',
-						border: activeQuestionId == index ? '1px solid white' : 'none',
-						borderTop: '1px solid white',
-						borderRadius: activeQuestionId == index ? '20px' : '0',
-					}}
-					className='flex items-center overflow-hidden transition-all duration-700 w-full text-start md:px-4 focus:outline-none gap-2  p-10'
-				>
-					<div
+				<>
+					<hr />
+					<button
+						key={index}
+						onClick={() => toggleTab(index)}
 						style={{
-							alignItems: item.reverse ? 'end' : 'start',
-							textAlign: item.reverse ? 'end' : 'start',
+							height: activeQuestionId === index ? '400px' : '80px',
+							flexDirection: item.reverse == true ? 'row-reverse' : 'row',
 						}}
-						className='w-full flex  flex-col justify-between gap-2'
+						className='flex items-center overflow-hidden transition-all duration-700 w-full text-start md:px-4 focus:outline-none gap-2  p-10 my-10'
 					>
-						<h4 className='font-bold text-lg '>{item.name}</h4>
-						<p>{item.role}</p>
-						<p
+						<div
 							style={{
-								opacity: activeQuestionId === index ? '1' : '0',
+								alignItems: item.reverse ? 'end' : 'start',
+								textAlign: item.reverse ? 'end' : 'start',
+								transform: `translateY(${
+									activeQuestionId === index ? '0px' : '94px'
+								})`,
 							}}
-							className='w-full md:w-1/2'
+							className='w-full flex mt-5 flex-col justify-between gap-2'
 						>
-							{item.text}
-						</p>
-					</div>
-					{item.img && (
-						<img
-							src={item.img}
-							className='hidden lg:flex rounded-[10px] object-cover transition-all duration-500'
-							style={{
-								height: activeQuestionId === index ? '320px' : '320px',
-							}}
-						/>
-					)}
-				</button>
+							<h4 className='font-bold text-lg '>{item.name}</h4>
+							<p>{item.role}</p>
+							<p
+								style={{
+									opacity: activeQuestionId === index ? '1' : '0',
+								}}
+								className='w-full md:w-1/2 transition-all duration-700'
+							>
+								{item.text}
+							</p>
+						</div>
+						{item.img && (
+							<img
+								src={item.img}
+								className='hidden lg:flex rounded-[10px] object-cover transition-all duration-500'
+								style={{
+									height: activeQuestionId === index ? '320px' : '320px',
+								}}
+							/>
+						)}
+					</button>
+				</>
 			))}
-		</>
+		</div>
 	);
 };
 TeamAccordion2.propTypes = {
